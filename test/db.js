c3df11f1fe58391057a0eb8cd27bf19f0dd7d87f
Mark Hansen <mhansen@accusoft.com>
2016-07-01 17:28:04 -0400

    Fixed unit tests

--- test/db.js
+++ test/db.js
@@ -6,13 +6,13 @@ var db = new database();
 describe("Database Manager", function() {
     it("Reads a Single Post from the Datastore", function() {
       var post = db.getPost(1);
-      expect(post).to.be.a('array');
-      expect(post[0].postTitle).to.equal('My First Blog Post');
+      expect(post).to.be.a('object');
+      expect(post.postTitle).to.equal('My First Blog Post');
     });
 
     it("Reads all posts in the database" , function () {
       var posts = db.getAllPosts(true);
-      expect(posts).to.be.an('object');
-      expect(posts.data).to.have.length(2);
+      expect(posts).to.be.an('array');
+      expect(posts).to.have.length.above(2);
     })
 });
