ecc96d83d6cab1ad7ba99468a3f17306113f61ca
Mark Hansen <mhansen@accusoft.com>
2016-07-06 09:26:07 -0400

    Pass datastore file name instead of hardcoded via settings

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ module.exports = function() {
     app.use(session(sessionOptions)); // let's use sessions
     app.use(bodyParser.urlencoded({extended: false })); // we need to be able to read form posts
     app.set('view engine', 'ejs'); // view engine duh
+    app.set('views',__dirname,'views');
     app.engine('ejs', engine); // use ejs-locals for all ejs templates:
     app.locals.moment = require('moment'); // makes working with time easier
     app.locals.textHelpers = require('../texthelpers'); // local helpers
